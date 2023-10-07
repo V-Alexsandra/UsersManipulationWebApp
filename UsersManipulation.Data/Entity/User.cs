86b1d62c97aaedb0d3e4350a45a8443497cb575f
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-10-07 19:26:48 +0100

    Initial commit

---
+++ UsersManipulation.Data/Entity/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersManipulation.Data.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Date of Registration")]
+        public DateTime RegistrationDate { get; set; }
+
+        [Display(Name = "Last Login Date")]
+        public DateTime LastLoginDate { get; set; }
+
+        public bool IsBlocked { get; set; }
+    }
+}
